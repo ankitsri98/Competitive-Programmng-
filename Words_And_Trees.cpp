e2f976cf07fd1568eb77c96de2abcc9b36025fee
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2020-04-04 05:09:01 +0530

    Update Words_And_Trees.cpp

--- Words_And_Trees.cpp
+++ Words_And_Trees.cpp
@@ -15,11 +15,15 @@ void dfs(int u){
             dfs(i);
             //2:-so now the changes in leaf node is made which will be updated in its parent u and the child is 'i'
             for(int j=0;j<26;j++){
-                tree[u][j]+=tree[i][j];//3:-its not necessary that this i always a leaf node , it can be a sub tree rooted with 'i' so all its changes will be reflected to this u by this loop
+                tree[u][j]+=tree[i][j];//3:-its not necessary that this i always a leaf node , 
+                //it can be a sub tree rooted with 'i' so all its changes will be reflected to this u by this loop
             }
         }
     }
-    tree[u][arr[u]-'a']++;//1:-here u is a leaf node so every node as we know represents a character so we update that char. at that node. After that it get back to its parent and now that parent get updated with the value of leaf node(its child) becoz the logic is that subtree rooted at parent will hold the complete length of string in its subtree
+    tree[u][arr[u]-'a']++;//1:-here u is a leaf node so every node as we know represents a character 
+    //so we update that char. at that node. After that it get back to its parent and now that parent 
+    //get updated with the value of leaf node(its child) becoz the logic is that subtree rooted at parent
+    //will hold the complete length of string in its subtree
 }
 int main(){
     ll n,q,x,y,z;
@@ -42,7 +46,7 @@ int main(){
             freq[s[i]-'a']++;
         }
         //now the tree that i have made in which each node is a root 
-        //and with all the char. that its subtree has basically                 maintained a frequency
+        //and with all the char. that its subtree has basically maintained a frequency
         for(int i=0;i<26;i++){
             ans+=max(0,freq[i]-tree[z-1][i]);
         }
