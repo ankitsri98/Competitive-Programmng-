1419020d46b9e4e07ef5a54d81ab7032909e4fb2
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2020-05-11 20:24:16 +0530

    Update Stock_Span_Problem.cpp

--- Interview/STACK/Stock_Span_Problem.cpp
+++ Interview/STACK/Stock_Span_Problem.cpp
@@ -1,5 +1,8 @@
-The stock span problem is a financial problem where we have a series of N daily price quotes for a stock and we need to calculate span of stock’s price for all N days. You are given an array of length N, where ith element of array denotes the price of a stock on ith. Find the span of stock's price on ith day, for every 1<=i<=N.
-A span of a stock's price on a given day, i, is the maximum number of consecutive days before the (i+1)th day, for which stock's price on these days is less than or equal to that on the ith day.
+The stock span problem is a financial problem where we have a series of N daily price quotes for a 
+stock and we need to calculate span of stock’s price for all N days. You are given an array of length N,
+where ith element of array denotes the price of a stock on ith. Find the span of stock's price on ith day, for every 1<=i<=N.
+A span of a stock's price on a given day, i, is the maximum number of consecutive days before the (i+1)th
+day, for which stock's price on these days is less than or equal to that on the ith day.
 
 Input Format
 First line contains integer N denoting size of the array.
