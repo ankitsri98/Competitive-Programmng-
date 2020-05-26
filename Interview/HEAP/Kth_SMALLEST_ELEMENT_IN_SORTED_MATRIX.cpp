fbcb38ab663adda3965e5f38c445a468673c82b4
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2020-05-27 05:17:50 +0530

    complexity N^2 LOG(K)

---
+++ Interview/HEAP/Kth_SMALLEST_ELEMENT_IN_SORTED_MATRIX.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        
+        priority_queue<int > pq;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[i].size();j++){
+                pq.push(matrix[i][j]);
+                if(pq.size()<=k){
+                    continue;
+                }else{
+                    pq.pop();
+                }
+            }
+        }
+        return pq.top();
+    }
+};
