670358befb25014f3e0ef17f5ed633f1fbd3e13f
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2020-09-18 12:02:00 +0530

    Create Runtime Polymorphism.cpp

---
+++ Interview/OOPS/Runtime Polymorphism.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class shape{
+public:
+	virtual void draw(){
+		cout<<"shapes"<<endl;
+	}	
+};
+class tri:public shape{
+public:
+	void draw(){
+		cout<<"triangle"<<endl;
+	}	
+};
+class rect:public shape{
+public:
+	void draw(){
+		cout<<"rect"<<endl;
+	}	
+};
+
+int main() {
+	shape*s;
+	shape ss;
+	tri t;
+	rect r;
+	s=&ss;
+	s->draw();
+	s=&t;
+	s->draw();
+	s=&r;
+	s->draw();
+	return 0;
+}
