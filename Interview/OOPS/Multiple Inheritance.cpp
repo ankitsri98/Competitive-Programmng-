22211f89f7da573771a01e8b1a1794d43a516302
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2020-09-18 10:09:15 +0530

    Create Multiple Inheritance.cpp

---
+++ Interview/OOPS/Multiple Inheritance.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class shape{
+public:
+	void A(){
+		cout<<"A"<<endl;
+	}	
+};
+class tri{
+public:
+	void B(){
+		cout<<"B"<<endl;
+	}	
+};
+
+class circle: public tri,public shape{
+public:
+	void C(){
+		cout<<"C"<<endl;
+	}	
+};
+int main() {
+	circle obj;
+	obj.A();
+	obj.B();
+	obj.C();
+	return 0;
+}
