f205445195a786e95a30ddfa4541f559d765a5ce
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2023-06-01 23:50:32 +0530

    Update total subarrays with odd or even sum.cpp

--- Interview/total subarrays with odd or even sum.cpp
+++ Interview/total subarrays with odd or even sum.cpp
@@ -1,6 +1,9 @@
-/*Firstly if a number is even then it would just increase even number of subarrays by 1 because the number itself becomes the new subarray which is added in even total. Even +Even=Even
+/*Firstly if a number is even then it would just increase even number of subarrays by 1 because 
+the number itself becomes the new subarray which is added in even total. Even +Even=Even
 The odd sum subarrays remain constant as the sum still remains constant. Odd+Even=Odd
-But when the new number is odd then comes the catch.Actually it makes all the even summed subarrays odd summed and odd summed to even summed.At the end since the number is odd it increases one odd subarray also.
+But when the new number is odd then comes the catch.Actually it makes all the even summed subarrays 
+odd summed and odd summed to even summed.At the end since the number is odd it increases one odd subarray also.
+
 Odd+Odd=Even
 Even +Odd=Odd
 Due to the above rules we need to swap odd and even then increase odd by 1.
