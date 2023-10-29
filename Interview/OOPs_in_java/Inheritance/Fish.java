9b74679875b62da18872b7b2c77e52bef86cc070
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2023-10-29 21:34:58 +0530

    Add files via upload

---
+++ Interview/OOPs_in_java/Inheritance/Fish.java
@@ -0,0 +1,13 @@
+package com.personal.oops;
+
+public class Fish extends Animal {
+
+    //Remember that the base and child class should have the same way of creating object like parameterised constructor here.
+    public Fish(int age, String gender, int weightInlbs) {
+        super(age, gender, weightInlbs);
+    }
+
+    public void swim() {
+        System.out.println("swimming...");
+    }
+}
