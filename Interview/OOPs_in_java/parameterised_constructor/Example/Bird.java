ec77a5252765b7ad9ab26a22579120149ed6c948
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2023-10-29 15:29:38 +0530

    Add files via upload

---
+++ Interview/OOPs_in_java/parameterised_constructor/Example/Bird.java
@@ -0,0 +1,8 @@
+package com.personal.oops;
+
+public class Bird {
+
+    public void fly() {
+        System.out.println("flying...");
+    }
+}
---
+++ Interview/OOPs_in_java/parameterised_constructor/Example/Fish.java
@@ -0,0 +1,8 @@
+package com.personal.oops;
+
+public class Fish {
+
+    public void swim() {
+        System.out.println("swimming...");
+    }
+}
