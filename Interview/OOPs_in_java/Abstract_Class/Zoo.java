0bc40d3d115ddc6b0879b5d259b77db06e45f74d
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2023-11-01 18:34:43 +0530

    Update Zoo.java

--- Interview/OOPs_in_java/Abstract_Class/Zoo.java
+++ Interview/OOPs_in_java/Abstract_Class/Zoo.java
@@ -20,7 +20,7 @@ public class Zoo {
         Animal b = new Bird(1, "f", 21);
         b.move();
 
-        Here instance variable is of Animal type but it points to Fish and bird.
+        Here instance variable is of Animal type but it points to Fish and bird. ALSO F. ALLOWS ONLY METHODS WRITTEN IN ANIMAL CALL EVEN IF OBJECT IS OF FISH OR BIRD IT CAN'T CALL FISH BIRD CLASS METHOD
 
         This is not possible 
         Animal f = new Animal(1,"m",13);
